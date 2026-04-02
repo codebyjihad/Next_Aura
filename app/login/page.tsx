b3f3099176bd3120ad08edf88d55cce8d5a92975
jihad <codebyjihad@gmail.com>
2026-04-02 21:31:32 +0600

    Feat: Project Install setup shadcn

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LogPages = () => {
+  return (
+    <div className='text-2xl font-bold text-center bg-background text-foreground border border-border'>
+        
+    </div>
+  )
+}
+
+export default LogPages
